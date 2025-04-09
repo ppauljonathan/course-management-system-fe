1c9d1964cd112a4fbdd07870c8ee1cba0a702221
P Paul Jonathan <ppauljonathan@gmail.com>
2025-04-09 10:37:29 +0530

    improve TruncatedText component

--- src/components/CourseList/CourseCard.tsx
+++ src/components/CourseList/CourseCard.tsx
@@ -1,7 +1,7 @@
 import CourseInterface from "../../interfaces/graphql/courses/courseInterface";
 import VideoPlaceHolder from "../../assets/video-placeholder.svg";
-import TruncateText from "../../utils/TruncateText";
 import { Link } from "react-router";
+import TruncatedText from "../TruncatedText";
 
 interface CourseCardPropsInterface {
   course: CourseInterface
@@ -40,17 +40,16 @@ function CourseCard({ course, showAdminData }: CourseCardPropsInterface) {
         />
       </div>
       <div className="ml-5">
-        <div className="text-lg font-bold">
-          <TruncateText
-            str={course.name}
-            maxLength={30}
-          />
-        </div>
-          {
-            course.price == 0 ?
-              <div>Free</div> :
-              <div>Price: ${course.price.toFixed(2)}</div>
-          }
+        <TruncatedText
+          str={course.name}
+          maxLength={30}
+          className="text-lg font-bold"
+        />
+        {
+          course.price == 0 ?
+            <div>Free</div> :
+            <div>Price: ${course.price.toFixed(2)}</div>
+        }
        </div>
 
       </div>
---
+++ src/components/TruncatedText.tsx
@@ -0,0 +1,31 @@
+interface TruncatedTextProps {
+  str: string;
+  maxLength?: number;
+  ending?: string;
+  className?: string
+}
+
+function truncate(str: string, maxLength = 20, ending = '...') {
+  if (str.length > maxLength) {
+    return str.slice(0, maxLength - ending.length) + ending;
+  }
+  return str;
+}
+
+function TruncatedText({str, className, maxLength = 20, ending = '...'}: TruncatedTextProps) {
+  const isTruncated = str.length > maxLength;
+
+  return (
+    <div className={`relative group ${className}`}>
+      <span>{ truncate(str, maxLength, ending) }</span>
+
+      {isTruncated && (
+        <div className="absolute bottom-full left-0 z-10 hidden w-max max-w-xs rounded bg-black p-2 text-xs text-white group-hover:block">
+          {str}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default TruncatedText
