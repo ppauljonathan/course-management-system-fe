1f40766bfc94d5b8ce2f087a66686f52580b4e38
P Paul Jonathan <ppauljonathan@gmail.com>
2025-04-15 17:22:15 +0530

    make description markdown with a preview, also a markdown tutorial

--- src/components/MarkdownInput.tsx
+++ src/components/MarkdownInput.tsx
@@ -1,5 +1,8 @@
 import { ChangeEvent, useState } from "react";
+import { Link } from "react-router";
 import ReactMarkdown from "react-markdown";
+import { ArrowLongDownIcon } from "@heroicons/react/24/solid";
+import { QuestionMarkCircleIcon } from "@heroicons/react/24/outline";
 
 import FormInputLabel from "./FormInputLabel";
 
@@ -19,28 +22,48 @@ function MarkdownInput({ name, labelName, required, value, onChange, errorMessag
     <>
       <div>
         <FormInputLabel name={name} labelName={labelName} required={required} />
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            setShowPreview(prev => !prev);
-          }}
-        >Show Preview</button>
-        {
-          !showPreview &&
-          <textarea
-            name={name}
-            className="border p-2 rounded w-full min-h-40"
-            value={value}
-            onChange={onChange}
-            {...(required ? { required : true } : {})}
-          />
-        }
-        {
-          showPreview &&
-           <div className="border p-2 rounded w-full prose dark:prose-invert">
-            <ReactMarkdown>{value}</ReactMarkdown>
+        <div className="p-2 border rounded">
+          <div className="flex">
+            <div
+              onClick={() => { setShowPreview(false); }}
+              className={`p-2 border rounded m-2 ${showPreview ? '' : 'bg-gray-200 dark:bg-gray-500' }`}
+            >Raw</div>
+            <div
+              onClick={() => { setShowPreview(true); }}
+              className={`p-2 border rounded m-2 ${showPreview ? 'bg-gray-200 dark:bg-gray-500' : '' }`}
+            >Preview</div>
+            <div className="group ml-auto">
+
+              <Link to="/md-help" target="_blank">
+                <QuestionMarkCircleIcon className="size-6" />
+              </Link>
+              <p className="absolute text-white bg-black p-2 rounded hidden group-hover:block">Markdown Tutorial</p>
+            </div>
           </div>
-        }
+          {
+            !showPreview &&
+            <textarea
+              name={name}
+              className="border p-2 rounded w-full min-h-40 max-h-60 overflow-y-scroll"
+              value={value}
+              onChange={onChange}
+              {...(required ? { required : true } : {})}
+            />
+          }
+          {
+            showPreview &&
+             <div className="border p-2 rounded w-full max-w-full min-h-40 prose dark:prose-invert max-h-60 overflow-y-scroll">
+              <ReactMarkdown>{value || "Nothing to Preview..."}</ReactMarkdown>
+            </div>
+          }
+          <div className="group">
+            <div className="w-fit p-0.5 border-2 rounded-sm font-bold flex justify-center mt-2">
+              <p className="text-sm">M</p>
+              <ArrowLongDownIcon className="size-4" />
+            </div>
+            <p className="absolute text-white bg-black p-2 rounded hidden group-hover:block">Markdown Input</p>
+          </div>
+        </div>
         {
           errorMessage &&
           <span className="text-red-800 dark:text-red-400">{errorMessage}</span>
