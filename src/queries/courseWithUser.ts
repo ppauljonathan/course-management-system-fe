f8c437229961556fe2a91734d7db17af81bfc734
P Paul Jonathan <ppauljonathan@gmail.com>
2025-04-29 20:46:07 +0530

    Add Course show page

---
+++ src/queries/courseWithUser.ts
@@ -0,0 +1,23 @@
+const courseWithUser = `
+  query FetchCourse($id: ID!) {
+    course(id: $id) {
+      id
+      name
+      description
+      price
+      created_at
+      updated_at
+      live
+      user {
+        id
+        first_name
+        last_name
+        email
+        created_at
+        updated_at
+      }
+    }
+  }
+`;
+
+export default courseWithUser;
